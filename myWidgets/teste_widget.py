2bfc4290d9376f60cec96d3fe88322fee93fdb5d
AndrewDRichter <andrewrichter1@hotmail.com>
2024-04-17 14:37:19 -0400

    sei la

--- myWidgets/teste_widget.py
+++ myWidgets/teste_widget.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
 class myButton:
-    def __init__(self, width, height):
+    def __init__(self, width, height, color, bg):
         self.size = (width, height)
+        self.color = color
+        self.bg_color = bg
--- real_python_tkinter/firstScreen.py
+++ real_python_tkinter/firstScreen.py
@@ -3,6 +3,8 @@ import tkinter as tk
 
 window = tk.Tk()
 greeting = tk.Label(text="Python rocks!")
+greeting["pady"] = 350
+greeting["padx"] = 500
 greeting.pack()
 
 window.mainloop()
