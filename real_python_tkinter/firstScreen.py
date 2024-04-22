8599edee8733d58482527c2e21c127339ee4e27f
AndrewDRichter <andrewrichter1@hotmail.com>
2024-04-22 16:05:31 -0400

    image feature testing in tkinter app

--- real_python_tkinter/firstScreen.py
+++ real_python_tkinter/firstScreen.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 
 window = tk.Tk()
+window.title("Bg setting")
+img = tk.PhotoImage(file="bg.png")
+tk.Label(window, image= img).pack()
 greeting = tk.Label(text="Python rocks!")
 greeting["pady"] = 350
 greeting["padx"] = 500
